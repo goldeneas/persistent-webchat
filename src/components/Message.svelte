924da8edd20598a468c5f6b886b5cab4c231809c
Nicola Picarella <rialhaxor@gmail.com>
2022-09-25 12:53:09 +0200

    svelte commit

---
+++ src/components/Message.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let text: string;
+    export let username: string;
+    export let timestamp: string;
+</script>
+
+<div class="bg-white border-2 rounded-md w-fit h-fit px-2">
+    <p>
+        <span class="font-medium">{username}</span>
+        <span class="font-normal">{text}</span>
+        <span class="font-extralight text-xs">{timestamp}</span>
+    </p>
+</div>
---
+++ src/components/MessageBoard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Message from "./Message.svelte";
+    import { getMessages } from "../scripts/database";
+    import { onMount } from "svelte";
+
+    let messages: message[] = [];
+</script>
+
+<div class="absolute border-4 w-[98vw] h-[94vh] left-3">
+    <svelte:component this={Message} text="as" username="asd" timestamp="asd" />
+</div>
